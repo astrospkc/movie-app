2bf7af8e255c044dcf331e35fe9eb0b673fa549d
astrospkc <punamkumari399@gmail.com>
2023-04-25 16:33:21 +0530

    search feature added

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -10,11 +10,11 @@ import { API_KEY, base_url } from "./urls";
 let genre_url = base_url + `/discover/movie?with_genres=28&` + API_KEY;
 let url = base_url + "/discover/movie?" + API_KEY;
 
-// https://api.themoviedb.org/3/discover/tv?sort_by=popularity.desc&api_key=11fb2ad6e2a4150cef0c8ce69ff49879
+// https://api.themoviedb.org/3/discover/tv?sort_by=popularity.desc&api_key=apikey
 const Movies = (props) => {
   const [data, setData] = useState([]);
   const [main_url, setUrl] = useState(url);
-
+  const [query, setQuery] = useState("");
   const getData = async (url) => {
     try {
       const res = await fetch(url);
@@ -43,27 +43,40 @@ const Movies = (props) => {
 
   return (
     <div>
-      <MovieSearch />
+      {/* search feature */}
+      <div className="flex flex-row justify-center items-center rounded-lg">
+        <input
+          className="px-4 py-3 rounded-lg mx-2 bg-slate-700"
+          type="text"
+          placeholder="Search movie , series "
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="bg-black text-white px-4 py-3 hover:bg-slate-500 rounded-lg">
+          Search
+        </button>
+      </div>
 
       <div className="">
         <div className="grid grid-cols-2 md:grid-cols-4 gap-x-10  m-2 py-4">
           {data &&
-            data.map((e, i) => {
-              const { title, release_date, imdb, vote_average, poster_path } =
-                e;
+            data
+              .filter((movie) => movie.title.toLowerCase().includes(query))
+              .map((e, i) => {
+                const { title, release_date, imdb, vote_average, poster_path } =
+                  e;
 
-              return (
-                <div className="col-md-3 " key={i}>
-                  <MovieCard
-                    title={title}
-                    release_date={release_date}
-                    vote_average={vote_average}
-                    imdb={imdb}
-                    imageurl={imageUrl + poster_path}
-                  />
-                </div>
-              );
-            })}
+                return (
+                  <div className="col-md-3 " key={i}>
+                    <MovieCard
+                      title={title}
+                      release_date={release_date}
+                      vote_average={vote_average}
+                      imdb={imdb}
+                      imageurl={imageUrl + poster_path}
+                    />
+                  </div>
+                );
+              })}
         </div>
       </div>
       {/* <Buttons /> */}
--- src/components/TvShows.jsx
+++ src/components/TvShows.jsx
@@ -10,6 +10,7 @@ let tv_url = base_url + "/discover/tv?&" + API_KEY;
 const TvShows = (props) => {
   const [tv_data, setTv_data] = useState([]);
   const [main_url, setMain_url] = useState(tv_url);
+  const [query, setQuery] = useState("");
 
   const getData = async (tv_url) => {
     try {
@@ -37,27 +38,45 @@ const TvShows = (props) => {
   return (
     <>
       <>
-        <MovieSearch />
+        {/* search feature */}
+        <div className="flex flex-row justify-center items-center rounded-lg">
+          <input
+            className="px-4 py-3 rounded-lg mx-2 bg-slate-700"
+            type="text"
+            placeholder="Search movie , series "
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          <button className="bg-black text-white px-4 py-3 hover:bg-slate-500 rounded-lg">
+            Search
+          </button>
+        </div>
         <div className="">
           <div className="grid grid-cols-2 md:grid-cols-4 gap-x-10  m-2 py-4">
             {tv_data &&
-              tv_data.map((e) => {
-                const { name, first_air_date, id, vote_average, poster_path } =
-                  e;
+              tv_data
+                .filter((data) => data.name.toLowerCase().includes(query))
+                .map((e) => {
+                  const {
+                    name,
+                    first_air_date,
+                    id,
+                    vote_average,
+                    poster_path,
+                  } = e;
 
-                return (
-                  <div className="col-md-3" key={id}>
-                    <TvCard
-                      name={name}
-                      first_air_date={first_air_date}
-                      vote_average={vote_average}
-                      imageurl={imageUrl + poster_path}
-                      id={id}
-                      // number_of_episodes={tv_url_ep + id + `?` + API_KEY}
-                    />
-                  </div>
-                );
-              })}
+                  return (
+                    <div className="col-md-3" key={id}>
+                      <TvCard
+                        name={name}
+                        first_air_date={first_air_date}
+                        vote_average={vote_average}
+                        imageurl={imageUrl + poster_path}
+                        id={id}
+                        // number_of_episodes={tv_url_ep + id + `?` + API_KEY}
+                      />
+                    </div>
+                  );
+                })}
           </div>
         </div>
         <Buttons />
